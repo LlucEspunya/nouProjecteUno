40281fb1218fa18d39e8c8d5ea4528567bdf0511
LlucEspunya <lespuna562@boscdelacoma.cat>
2025-05-07 14:50:06 +0200

    comentaris

--- Partida.java
+++ Partida.java
@@ -41,21 +41,21 @@ public class Partida {
 
             while(!cartaValida){
                 System.out.println("Última carta: ");
-                UI.mostrarCarta(pilo.consultarCarta());
+                UI.mostrarCarta(pilo.consultarCarta());                 //mostra la carta del pilo
 
-                tensCartaValida();
-                while(!tensCartaValida()){
+                tensCartaValida();                                      //abans de mostrar les cartes de la ma crido la funció "tensCartaValida"
+                while(!tensCartaValida()){                              //per comprovar si pot jugar amb les cartes de la ma, en cas de que no entre en el bucle
                     System.out.println("Robant cartes...");
                     jugadorActiu.robarCarta(mazo);
-                    if (mazo.getCartes().isEmpty()){
+                    if (mazo.getCartes().isEmpty()){                    //si el mazo està bui crida la funció "reiniciarMazo"
                         mazo.reiniciarMazo(pilo);
                     }
                 }
                 System.out.println("Cartes a la mà:");
                 UI.mostrarCartesJugador(ordreJugadors);
-                int cartaTirar = UI.triarCartaTirar(jugadorActiu.getCartes());
+                int cartaTirar = UI.triarCartaTirar(jugadorActiu.getCartes());      //crida la funció de la UI que demana la carta al jugador
 
-                if(jugadorActiu.potTirarCarta(pilo, cartaTirar)){
+                if(jugadorActiu.potTirarCarta(pilo, cartaTirar)){                   //crida la funció "potTirarCarta" per validar la carta que es vol jugar
                     jugadorActiu.tirarCarta(pilo, cartaTirar);
                     cartaValida = true;
                 }
@@ -74,7 +74,7 @@ public class Partida {
         return jugarPartida;
     }
 
-    private boolean tensCartaValida(){
+    private boolean tensCartaValida(){                  //comprova totes les cartes de la teva ma
         boolean cartesValides = false;
         for (int i = 0; i < ordreJugadors.getJugadorActiu().nombreDeCartes() && !cartesValides; i++){
             if (ordreJugadors.getJugadorActiu().potTirarCarta(pilo, i)){
